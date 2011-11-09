f4799237fee59687c5d4d9d30b7038501daef42c
Joe Mathes <joe@saucelabs.com>
2011-11-09 09:05:59 -0800

    support more

--- modules/gen_names.py
+++ modules/gen_names.py
@@ -10,24 +10,26 @@ import names
 
 
 def name(phenny, input): 
-   origterm = input.groups()[1]
-   if not origterm: 
-      return phenny.say('usage: name <gender>')
-   term = origterm.encode('utf-8')
-
-   term = term.lower()
-   if term not in ['male', 'female', 'random']:
-      return phenny.say('gender must be male, female, or random. other genders not supported, pervert')
-   
-   if term == 'random':
-      term = random.choice(['male, female'])
-   if term == 'male':
-      return phenny.say(names.get(True))
-   return phenny.say(names.get(False))
+    origterm = input.groups()[1]
+#    try:
+    if True: 
+        if not origterm: 
+            return phenny.say('usage: name <gender>')
+        term = origterm.encode('utf-8')
+        term = term.lower()
+        supported_types = names.supported_types()
+        if term not in supported_types:
+            error = 'gender must be ' + ', '.join(supported_types[:-1]) + ', or ' + supported_types[-1] + '. other genders not supported, pervert'
+            if term in error:
+                return phenny.say('ten points from gryffindor')
+            return phenny.say(error)
+        return phenny.say(names.get(term))
+#    except:
+#        return phenny.say('fifty points from gryffindor')
 
 
 name.commands = ['name']
 name.priority = 'high'
 
 if __name__ == '__main__': 
-   print __doc__.strip()
+    print __doc__.strip()
